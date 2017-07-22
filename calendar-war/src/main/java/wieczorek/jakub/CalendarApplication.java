da8d82f3ff425d48ec48b8cc2676c32405c06baa
jakubwieczorek <jakub.wieczorek0101@gmail.com>
2017-07-22 12:45:33 +0200

    jboss all in one package

--- calendar-war/src/main/java/wieczorek/jakub/CalendarApplication.java
+++ calendar-war/src/main/java/wieczorek/jakub/CalendarApplication.java
@@ -3,13 +3,25 @@ package wieczorek.jakub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CalendarApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
+public class CalendarApplication extends SpringBootServletInitializer
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(CalendarApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder calendarApplication)
+	{
+		return calendarApplication.sources(CalendarApplication.class);
 	}
 }
