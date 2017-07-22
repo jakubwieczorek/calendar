c2b8f47085b1efae86461d3f667133aeedf68cfa
jakubwieczorek <jakub.wieczorek0101@gmail.com>
2017-07-22 12:25:10 +0200

    test

--- calendar-war/src/main/java/wieczorek/jakub/CalendarApplication.java
+++ calendar-war/src/main/java/wieczorek/jakub/CalendarApplication.java
@@ -8,12 +8,21 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
-public class CalendarApplication
+public class CalendarApplication extends SpringBootServletInitializer
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(CalendarApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder calendarApplication)
+	{
+		return calendarApplication.sources(CalendarApplication.class);
 	}
 }
--- calendar-war/src/test/java/wieczorek/jakub/calendar/springboot/test/CalendarApplicationTest.java
+++ calendar-war/src/test/java/wieczorek/jakub/calendar/springboot/test/CalendarApplicationTest.java
@@ -1,18 +1,15 @@
 package wieczorek.jakub.calendar.springboot.test;
 
-import junit.framework.JUnit4TestAdapterCache;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.internal.runners.JUnit44RunnerImpl;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class
-CalendarApplicationTest {
+public class CalendarApplicationTest {
 
 	@Test
 	public void contextLoads() {
