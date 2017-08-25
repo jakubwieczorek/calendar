27f3616ae259e0c8aa73a3c091ce7f1e539d54fc
jakubwieczorek <jakub.wieczorek0101@gmail.com>
2017-08-25 22:02:42 +0200

    admincontroller without strings

--- calendar-war/src/main/java/wieczorek/jakub/controllers/AdminController.java
+++ calendar-war/src/main/java/wieczorek/jakub/controllers/AdminController.java
@@ -19,24 +19,20 @@ import java.util.Map;
 @RequestMapping("/admin")
 public class AdminController
 {
-    private static final String OK = "resource created successfully";
-    private static final String CONFLICT = "conflict";
-    private static final String NO_CONTENT = "User don't exist";
-
     @Autowired
     private PersonService personService;
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public ResponseEntity<String> createUser(@RequestBody PersonDTO aUser)
+    public ResponseEntity<Void> createUser(@RequestBody PersonDTO aUser)
     {
         if(personService.findUser(new PersonParam(aUser.getMail())) == null)
         {
             personService.addUser(aUser);
 
-            return new ResponseEntity<>(OK, HttpStatus.CREATED);
+            return new ResponseEntity<>((Void) null, HttpStatus.CREATED);
         }
 
-        return new ResponseEntity<>(CONFLICT, HttpStatus.CONFLICT);
+        return new ResponseEntity<>((Void) null, HttpStatus.CONFLICT);
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
@@ -46,28 +42,28 @@ public class AdminController
     }
 
     @RequestMapping(value = "/{mail:.+}", method = RequestMethod.PUT)
-    public ResponseEntity<String> updateUser(@PathVariable(value = "mail") String aMail, @RequestBody PersonDTO aUser)
+    public ResponseEntity<Void> updateUser(@PathVariable(value = "mail") String aMail, @RequestBody PersonDTO aUser)
     {
         PersonDTO userToUpdate = this.personService.findUser(new PersonParam(aMail));
 
         if(userToUpdate == null)
         {
-            return new ResponseEntity<>(NO_CONTENT, HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>((Void) null, HttpStatus.NO_CONTENT);
         }
 
         if(this.personService.findUser(new PersonParam(aUser.getMail())) != null && !aUser.getMail().equals(aMail)) // mail conflict
         { // mail is busy
-            return new ResponseEntity<>(CONFLICT, HttpStatus.CONFLICT);
+            return new ResponseEntity<>((Void) null, HttpStatus.CONFLICT);
         } else
         {
             this.personService.updateUser(new PersonParam(aMail), aUser);
 
-            return new ResponseEntity<>(OK, HttpStatus.OK);
+            return new ResponseEntity<>((Void) null, HttpStatus.OK);
         }
     }
 
     @RequestMapping(value = "/{mail:.+}", method = RequestMethod.DELETE)
-    public ResponseEntity<String> deleteUser(@PathVariable(value = "mail") String aMail)
+    public ResponseEntity<Void> deleteUser(@PathVariable(value = "mail") String aMail)
     {
         PersonDTO toUpdate = this.personService.findUser(new PersonParam(aMail));
 
@@ -75,9 +71,9 @@ public class AdminController
         {
             this.personService.deleteUser(toUpdate);
 
-            return new ResponseEntity<>(OK, HttpStatus.OK);
+            return new ResponseEntity<>((Void) null, HttpStatus.OK);
         }
 
-        return new ResponseEntity<>(NO_CONTENT, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>((Void) null, HttpStatus.NO_CONTENT);
     }
 }
